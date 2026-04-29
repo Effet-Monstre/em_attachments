94af2d7db36a69aaf16963918cbf625ddd88c27b
simon-em <163315302+simon-em@users.noreply.github.com>
2026-04-29 13:52:22 +0000

    Deploying to gh-pages from @ Effet-Monstre/em_attachments@06765bd27e00a197ecf9c03442a7de35abf4b344 🚀

--- test/support/test_router.ex
+++
@@ -1,8 +0,0 @@
-if Code.ensure_loaded?(Phoenix.Router) do
-  defmodule EmAttachments.Test.Router do
-    use Phoenix.Router, helpers: false
-
-    forward "/upload", EmAttachments.Plug.Upload,
-      uploader: EmAttachments.Test.BasicUploader
-  end
-end
